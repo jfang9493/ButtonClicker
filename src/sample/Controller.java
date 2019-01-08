18f72c79ee04f30787ced322ff3ea703aa71cd2e
jfang9493 <jfang9493@bths.edu>
2019-01-08 01:21:02 -0500

    ejhjytj

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,11 +6,15 @@ import javafx.scene.control.Label;
 public class Controller {
     public Label clicksText;
     private Integer numClicks = 0;
+    private boolean gameStart = false;
 
     public void buttonClicked(ActionEvent actionEvent)
     {
+        if(!gameStart)
+        {
+            gameStart = true;
+        }
         numClicks++;
         clicksText.setText("Number of Clicks: " + numClicks.toString());
     }
-
 }
